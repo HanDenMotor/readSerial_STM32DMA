33b591c59b352132be791f9c800bf6d4a522a59e
HanDen <45811537+HanDenMotor@users.noreply.github.com>
2019-09-30 00:27:00 +0900

    Add files via upload

---
+++ readSerial_STM32DMA.c
@@ -0,0 +1,46 @@
+//STM32のUARTをDMAで受信しArudinoのSerialライブラリのように使うやつ
+
+//本サンプルコードはUART2で作成しているため別の番号のUARTを使う場合は適時UARTのハンドラの番号を変更してください
+
+//DMA開始コマンド
+//以下の1行はループ処理が始まる前に入れてください
+//基本的には以下のようにUSER CODE BEGIN 2とUSER CODE END 2の間に入れると良いでしょう
+
+  /* USER CODE BEGIN 2 */
+  HAL_UART_Receive_DMA(&huart2,serialData,DATANUM);
+  /* USER CODE END 2 */
+
+
+//未読データ数確認関数
+//返り値は未読データ数
+int readDataNum(){//受信済みデータ数確認
+	int index = huart2.hdmarx->Instance->NDTR; //index取得
+
+	index = DATANUM - index;//受信データの先頭位置
+
+	int remainData = index - indexRead;//読み込んでいないデータの数
+	if(remainData < 0) remainData += DATANUM;
+	return remainData;
+}
+
+
+//受信データ1byte受信関数
+//実行ごとに古いデータから読みだされる
+//返り値は受信データ
+uint8_t readSerial(){//データ受信
+	uint8_t readData = 0;
+	
+	int index = huart2.hdmarx->Instance->NDTR; //index取得
+	index = DATANUM - index;
+
+	int remainData = index - indexRead;//読み込んでいないデータの数
+	if(remainData < 0) remainData += DATANUM;
+
+	if(remainData > 0){
+		readData = serialData[indexRead];
+		indexRead++;
+		if(indexRead == DATANUM) indexRead = 0;
+	}
+	
+	return readData;
+}
